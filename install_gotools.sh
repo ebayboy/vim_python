a72ab7a10f608f3e8270936609345272f3bb9956
fanpengfei1 <fanpengfei@jd.com>
2021-09-15 16:26:00 +0800

    fw

---
+++ install_gotools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x 
+
+go install golang.org/x/lint@latest
+go install github.com/ramya-rao-a/go-outline@latest
+go install github.com/acroca/go-symbols@latest
+go install github.com/josharian/impl@latest
+go install github.com/rogpeppe/godef@latest
+go install github.com/sqs/goreturns@latest
+go install github.com/golang/lint/golint@latest
+go install github.com/cweill/gotests/gotests@latest
+go install github.com/ramya-rao-a/go-outline@latest
+go install github.com/acroca/go-symbols@latest
+go install github.com/josharian/impl@latest
+go install github.com/rogpeppe/godef@latest
+go install github.com/sqs/goreturns@latest
+go install github.com/cweill/gotests/gotests@latest
