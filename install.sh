d392a71040e268dd437a33fa7cf782db20a8d1d5
fanpengfei1 <fanpengfei@jd.com>
2021-09-15 15:13:04 +0800

    fw

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #/bin/bash -x
 go get -v golang.org/x/tools/gopls
+go get -u github.com/jstemmer/gotags
 go get -u golang.org/x/tools/...
 ln -s $GOPATH  ~/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/
+mkdir -p /root/github.com/vim_python/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/go/bin/
+cp /usr/local/go/bin/gopls  /root/github.com/vim_python/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/go/bin/
